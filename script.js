63f8656e72545b55b75acb713364a4fa43966231
ShemeerCoder-01 <118423467+ShemeerCoder-01@users.noreply.github.com>
2023-03-14 22:42:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
-var yourArray = new Array(5);
-yourArray[0] = 'shemeer';
-yourArray[1] = 24;
-yourArray[2] = true;
-yourArray[4] = 'Accio';
-yourArray[5] = 'F-2';
+var yourArray = ['shemeer',24,'Accio',true,'F-2'];
+
